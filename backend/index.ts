16d932d9e5b7e06712350aa9f071b426bedf8df6
geekysilento <adityakarna.ak@gmail.com>
2024-01-04 23:21:34 +0545

    checkpoint-backend implementation

---
+++ backend/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose'
+import cors from 'cors'
+import authRoutes from './routes/auth';
+import todoRoutes from './routes/todo';
+const app = express();
+const port = 3000;
+const password = 135792468
+
+app.use(cors());
+app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/todo", todoRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
+mongoose.connect(`mongodb+srv://adityakarna:${password}@cluster0.oqkzqfh.mongodb.net/`)
---
+++ backend/middleware/index.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+const SECRET = 'SECr3t'; // This should be in an environment variable in a real application
+
+interface User {
+  id: string;
+}
+
+const authenticateJwt = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+      const token = authHeader.split(' ')[1];
+      jwt.verify(token, SECRET, (err, user) => {
+        if (err) {
+          return res.sendStatus(403);
+        }
+        req.userId = user.id;
+        next();
+      });
+    } else {
+      res.sendStatus(401);
+    }
+  };
+
+
+export { authenticateJwt, SECRET };
