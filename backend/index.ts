26d775dfd071e721f5d0fda2916d1d76bb06d4b1
geekysilento <125768789+geekysilento@users.noreply.github.com>
2024-01-15 00:04:08 +0545

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -13,7 +13,7 @@ app.use("/auth", authRoutes);
 app.use("/todo", todoRoutes);
 
 app.get('/', (req, res) => {
-    res.send('Hello, Express!. My server runs well!');
+    res.send('Hello, World!. My server runs well!');
 });
 
 app.listen(port, () => {
